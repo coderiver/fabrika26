e1c8d876b0cb5a4e37067badf10084c09d593e3c
Valera19900 <kaktuz34@gmail.com>
2014-11-14 11:29:13 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,8 +1,45 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	var gallery = $('.js-gallery'),
+		gallery_slider_all = gallery.find('.slider');
+	gallery.each(function () {
+		var gallery_this = $(this),
+			gallery_slider = gallery_this.find('.slider'),
+			gallery_list = gallery_slider.find('.slider__wrap'),
+			gallery_prev = gallery_slider.find('.slider__prev'),
+			gallery_next = gallery_slider.find('.slider__next'),
+			gallery_item = gallery_slider.find('.slider__item'),
+			gallery_close = gallery_slider.find('.slider__close'),
+			gallery_trigger = gallery_this.find('.items .item');
+		gallery_trigger.on('click', function () {
+			if (!gallery_this.hasClass('is-visible')) {
+				gallery.removeClass('is-visible');
+				gallery_slider_all.slideUp();
+				gallery_this.addClass('is-visible');
+				gallery_slider.slideDown(400, function () {
+					var gallery_top = gallery_this.offset().top;
+					$('html, body').animate({
+						scrollTop: gallery_top
+					}, 400);
+				});
+			};
+			var index = $(this).index();
+			gallery_list.cycle('goto', index);
+		});
+		if (gallery.length) {
+			gallery_list.cycle({
+				fx: 'scrollHorz',
+				timeout: 0,
+				slides: gallery_item,
+				prev: gallery_prev,
+				next: gallery_next
+			});
+		};
+		gallery_close.on('click', function () {
+			gallery_slider.removeClass('is-visible');
+			gallery_slider.slideUp();
+		});
+	});
+		
 
-	console.log($('body').html());
 });
