a411a78ab2369cb806206cfcf9ccccf23cba6dcf
Valera19900 <kaktuz34@gmail.com>
2014-11-18 14:39:03 +0200

    tabs

--- js/common.js
+++ js/common.js
@@ -63,4 +63,25 @@ head.ready(function() {
 		}, 600);
 		return false;
 	});
+
+	//tabs
+	function tab() {
+		$(".js-tab").each(function(){
+			var tab_link = $(this).find("a");
+			var tab_item = $(this).find("li");
+			var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+			// tab_cont.hide();
+			tab_item.first().addClass("is-active");
+			$(this).parents(".js-tab-group").find(".js-tab1").addClass("is-active");
+			tab_link.on("click", function() {
+				var index = $(this).attr("href");
+				tab_item.removeClass("is-active");
+				$(this).parent().addClass("is-active");
+				tab_cont.removeClass('is-active')
+				$(this).parents(".js-tab-group").find("."+index).addClass("is-active");
+				return false;
+			});
+		});
+	}
+	tab();
 });
